ea86a7a39861c6c9b9d65212c74350bf58d1de30
Your name here <rupi2003nder@gmail.com>
2021-06-03 21:20:05 +0530

    myfirstcommit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+   <>
+   <h1>My Favourite Netflix Picks</h1>
+   <p>here the list of my favourite 5 netfix series</p>
+   <ol>
+    <li>Dark</li>
+    <li>REd</li>
+    <li>Yellow</li>
+    <li>Green</li>
+    <li>Grey</li>
+
+   </ol>
+   </>
+    ,document.getElementById("root")
+);
